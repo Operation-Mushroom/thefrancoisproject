14640690b55a83b3180912bccc281cb44627391d
parthpd24 <parthpd24@gmail.com>
2015-03-31 12:20:27 -0400

    Update Data.java
    Started JFrame for application

--- Data.java
+++ Data.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
+import java.awt.*;
+import javax.swing.*;
 
 class Data {
 	public Scanner userInput = new Scanner(System.in);
+	JFrame mainFrame = new JFrame("Francois");
+	mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	public void createFrame(input, output) {
+		
+	}
 }
